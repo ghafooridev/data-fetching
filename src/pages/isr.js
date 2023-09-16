b27524703b85f0f1e46a4ab048e818bd5897baab
ghafooridev <ghafooridev@gmail.com>
2023-09-16 16:52:25 +0330

    first init

--- src/pages/isr.js
+++ src/pages/isr.js
@@ -4,9 +4,9 @@ import { API_URL } from '@/constants';
 export default function IncrementalStaticRegeneration({ data }) {
   return (
     <>
-      {data.map((e) => (
+      {/* {data.map((e) => (
         <h2 key={e.id}>{e.name}</h2>
-      ))}
+      ))} */}
     </>
   );
 }
@@ -14,18 +14,19 @@ export default function IncrementalStaticRegeneration({ data }) {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const res = await fetch(API_URL);
-  const data = await res.json();
+// export async function getStaticProps() {
+//   const res = await fetch(API_URL);
+//   const data = await res.json();
 
-  return {
-    props: {
-      data, // will be passed to the page component as props
-    },
+//   return {
+//     props: {
+//       data, // will be passed to the page component as props
+//     },
 
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
-    revalidate: 30, // In seconds
-  };
-}
+//     // Next.js will attempt to re-generate the page:
+//     // - When a request comes in
+//     // - At most once every second
+//     revalidate: 30, // In seconds
+//     fallback:false
+//   };
+// }
