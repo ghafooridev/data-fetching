0157ecc20775c07a5ada98498d181567a237e87f
ghafooridev <ghafooridev@gmail.com>
2023-09-16 17:09:49 +0330

    add ssg/isr

--- src/pages/isr.js
+++ src/pages/isr.js
@@ -1,12 +1,13 @@
 // incremental static generation
 import { API_URL } from '@/constants';
 
-export default function IncrementalStaticRegeneration({ data }) {
+export default function IncrementalStaticRegeneration(props) {
+  const data=props?.data;
   return (
     <>
-      {/* {data.map((e) => (
+      {data.map((e) => (
        <h2 key={e.id}>{e.title}</h2>
-      ))} */}
+      ))}
     </>
   );
 }
@@ -14,19 +15,18 @@ export default function IncrementalStaticRegeneration({ data }) {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
-// export async function getStaticProps() {
-//   const res = await fetch(API_URL);
-//   const data = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
 
-//   return {
-//     props: {
-//       data, // will be passed to the page component as props
-//     },
+  return {
+    props: {
+      data, // will be passed to the page component as props
+    },
 
-//     // Next.js will attempt to re-generate the page:
-//     // - When a request comes in
-//     // - At most once every second
-//     revalidate: 30, // In seconds
-//     fallback:false
-//   };
-// }
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every second
+    revalidate: 30, // In seconds
+  };
+}
--- src/pages/ssg.js
+++ src/pages/ssg.js
@@ -5,9 +5,9 @@ export default function StaticSiteGeneration(props) {
   const data=props?.data;
   return (
     <>
-      {/* {data.map((e) => (
+      {data.map((e) => (
         <h2 key={e.id}>{e.title}</h2>
-      ))} */}
+      ))}
     </>
   );
 }
@@ -15,13 +15,13 @@ export default function StaticSiteGeneration(props) {
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
-// export async function getStaticProps() {
-//   const res = await fetch(API_URL);
-//   const data = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
 
-//   return {
-//     props: {
-//       data, // will be passed to the page component as props
-//     },
-//   };
-// }
+  return {
+    props: {
+      data, // will be passed to the page component as props
+    },
+  };
+}
